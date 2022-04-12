898e4a2fe34e5dc64b5c8bbc17d95a6f3ecbcd98
xanxad <seifubiniam6@gmail.com>
2022-04-12 07:00:35 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *sh = "Holberton";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+
+_putchar('\n');
+
+return (0);
+}
