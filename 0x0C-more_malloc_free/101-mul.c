0f44fb54ee21b3be430781fb5e3173ae5e280557
xanxad <seifubiniam6@gmail.com>
2022-05-06 03:39:55 -0700

    tassk 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
+
 if (argc != 3)
 { printf("Error\n");
 exit(98); }
@@ -23,7 +24,7 @@ if (argv[i][j] > 57 || argv[i][j] < 48)
 exit(98); }
 }
 }
-mul = atol(argv[1]) *atol(argv[2]);
+mul = atol(argv[1]) *  atol(argv[2]);
 printf("%lu\n", mul);
 return (0);
 }
