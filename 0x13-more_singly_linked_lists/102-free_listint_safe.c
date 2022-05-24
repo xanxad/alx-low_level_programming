2509ba9427bdca00e8c3ba7455265a5b1d720405
xanxad <seifubiniam6@gmail.com>
2022-05-24 15:55:30 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,37 +1,37 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * free_listint_safe - thsi function free a list in safe mode
- * @h: the head of list
- * Description: this function free a string in a safe mode
- * section header: the header of this function is lists.h)*
- * Return: the size of the list
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *tmp, *actual;
-size_t i;
-int rest;
-i = 0, actual = *h;
+size_t len = 0;
+int diff;
+listint_t *temp;
 
-while (actual)
+if (!h || !*h)
+return (0);
+while (*h)
 {
-rest = actual - actual->next;
-if (rest > 0)
+diff = *h - (*h)->next;
+if (diff > 0)
 {
-tmp = actual->next;
-free(actual);
-actual = tmp;
-i++;
-} else
+temp = (*h)->next;
+*h = temp;
+len++;
+}
+else
 {
-free(actual);
 *h = NULL;
-i++;
+len++;
 break;
 }
 }
 *h = NULL;
-return (i);
+return (len);
 }
